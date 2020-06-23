d293cbca123c03400d3bc020108cc0a52da6da96
Yuhan (Douglas) Rao <38257268+geo-yrao@users.noreply.github.com>
2020-06-23 06:39:05 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,2 @@
-install.packages(c("readr","tidyr","ggplot2"))
+## This file install required R packages for the tutorials when the repo is first binderized
+install.packages(c("readr","tidyr","ggplot2","caret","kknn","LiblineaR","dplyr","magrittr"))
